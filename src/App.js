b12ecced995dacc0d3f5fe9d9bf6e17c4f652790
omar <chaweli10@gmail.com>
2020-10-16 15:27:15 +0100

    the last

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import myImage from './Innovation-strategique.jpg'
 import myVid from './myvideo.mp4'
 function App() {
   return (
     <>
-    <div style={{border:"solid 1px black",maxWidth:'100vw'}}>
+    <div style={{border:"solid 1px black",maxWidth:'100vw'}} className='image-position'>
       <h1 className="title red">Your name here</h1>
       <br/>
-      <img src={myImage} alt="brain"/>
+      <img className='image-design' src={myImage} alt="brain"/>
       <br/>
-      <img src='./images.jpg' alt="barinn"/>
+      <img className='image-design' src='./images.jpg' alt="barinn"/>
     </div>
-    <video style={{width:320,height:240}} controls>
+    <video style={{width:320,height:240,margin:'0 40%'}} controls>
       <source src={myVid} type='video/mp4'></source>
     </video>
     </>
